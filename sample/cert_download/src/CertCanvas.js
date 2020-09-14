ec97dca4d6fdba5899076331140063099b178c61
remagart <jfmamjjasond418@gmail.com>
2020-09-14 12:01:55 +0800

    [Add] Draw Text
    20200914

--- sample/cert_download/src/CertCanvas.js
+++ sample/cert_download/src/CertCanvas.js
@@ -7,6 +7,7 @@ const CertCanvas = (props) => {
     const [OutputPic, setOutputPic] = useState("");
 
     const handleCanvas = (canvas) => {
+        let userName = "王小明";
         if(canvas){
             // 畫畫囉
             canvas.width = 1558;
@@ -17,8 +18,22 @@ const CertCanvas = (props) => {
             image.addEventListener('load',async () => {
                 // context在上面宣告過了
                 context.drawImage(image, 0, 0, 1558, 1100);	// 與上面畫布大小設定一樣
-            
+                
+                context.font = 'bold 90px Microsoft JhengHei';
+                context.fillStyle = 'rgba(120, 123, 105, 0.5)';
+                context.fillText(userName, 105, 525);
+                const userNameMeasure = await context.measureText(userName);
+                const usernameWidth = userNameMeasure && userNameMeasure.width;
 
+                // 下面會使用到 usernameWidth 的長度來畫畫
+                context.beginPath()
+                context.moveTo(95, 555)		                        // 畫線開始位置
+                context.lineTo(95 + usernameWidth + 42, 555)		// 畫線結束位置
+                context.lineWidth = 3.5;
+                context.strokeStyle = 'rgba(120, 123, 105, 0.5)';
+                context.stroke();		                            // 開始畫線
+
+                /****** 輸出 *******/
                 let output = await canvas.toDataURL("image/jpeg"); // png也行
                 // output就會是一個base64的圖片了
 
